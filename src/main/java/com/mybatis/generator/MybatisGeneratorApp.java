5a23cdaa9f159f0316c99629c688b6ad6454f076
Tran Ngoc Nhan <ngocnhan.tran1996@gmail.com>
2022-07-28 22:52:39 +0700

    do: add execute app

---
+++ src/main/java/com/mybatis/generator/MybatisGeneratorApp.java
@@ -0,0 +1,10 @@
+package com.mybatis.generator;
+
+import java.io.IOException;
+
+public class MybatisGeneratorApp {
+
+  public static void main(String[] args) throws IOException {
+    Runtime.getRuntime().exec("cmd.exe /c mvn mybatis-generator:generate");
+  }
+}
